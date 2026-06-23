511f93b693beb83d0f5751fd4bf0e77af52daab3
Ch3my <joseconchan1@gmail.com>
2026-06-23 12:12:15 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,9 @@ export default defineConfig(async () => ({
       },
     },
   },
+  optimizeDeps: {
+    include: ["react-image-file-resizer"],
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
